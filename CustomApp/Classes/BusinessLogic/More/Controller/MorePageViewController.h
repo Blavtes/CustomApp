7c58dfc31d9ccb26a9fbf26fc132eb6b6d452536
Blavtes <Blavtes@gjfaxdeMacBook-Pro-4.local>
2017-04-17 15:43:25 +0800

    init custom app。

---
+++ CustomApp/Classes/BusinessLogic/More/Controller/MorePageViewController.h
@@ -0,0 +1,18 @@
+//
+//  MorePageViewController.h
+//  HX_GJS
+//
+//  Created by litao on 16/1/25.
+//  Copyright © 2016年 GjFax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  更多页面
+ */
+@interface MorePageViewController : CustomBaseViewController
+@property(nonatomic,strong)UILabel *redPointLabel0;//系统公告
+@property(nonatomic,strong)UILabel *redPointLabel1;//新闻动态
+@property(nonatomic,strong)UILabel *redPointLabel2;//活动中心
+@end
